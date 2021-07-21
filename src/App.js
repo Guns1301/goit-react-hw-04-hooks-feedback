6d994cec576aaa7379d18935d7b3fdd774b66aa5
Guns1301 <67137938+Guns1301@users.noreply.github.com>
2021-07-21 13:34:26 +0300

    ♻️ use hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { useState } from "react"; //для того что б хранить состояние(state) в функциях , надо
 import Section from "./Section/Section.js";
 import Statistics from "./Statistics/Statistics.js";
 import FeedbackOptions from "./FeedbackOptions/FeedbackOptions.js";
@@ -6,67 +6,61 @@ import Notification from "./Notification/Notification.js";
 
 import "./App.css";
 
-//чтобы превратить функцию в класс нужно из React унаследовать Component
-class App extends Component {
-  //static defaultProps = {}; и static PropTypes = {};
-  // в Классах defaultPropsоп и PropTypes описывается внутри класса ,
-  // после идет state = {}; затем кастомные методы () и последним метод render(){return};
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const countFeedbacks = (e) => {
+    const name = e.currentTarget.name;
+    switch (name) {
+      case "good":
+        setGood((prevgood) => good + 1);
+
+        break;
+      case "neutral":
+        setNeutral((prevneutral) => neutral + 1);
+
+        break;
+      case "bad":
+        setBad((prevbad) => bad + 1);
+
+        break;
+
+      default:
+        alert("Don`t work - try again");
+        break;
+    }
   };
 
-  countFeedback = (event) => {
-    const { name } = event.currentTarget;
-
-    this.setState((prevState) => ({
-      [name]: prevState[name] + 1, // в prevState реакт передаст во время обновления актуальное состояние объекта state. эта функция должна вернуть обновленный state
-    }));
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good / countTotalFeedback()) * 100) || 0;
   };
 
-  // что бы привязать контекст нужно создать публичный метод класса.
-  countTotalFeedback = () => {
-    return this.state.good + this.state.neutral + this.state.bad;
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    return this.countTotalFeedback()
-      ? Math.round((this.state.good / this.countTotalFeedback()) * 100)
-      : 0;
-  };
-  // Необходимо объявить обязательный метод render(), который вызывается по умолчанию и возвращает JSX-разметку.
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
-
-    return (
-      //тут и будем рендерить разметку
-      <div className="wrapper">
-        <Section title="Please leave your feedback about Espresso Cafe">
-          <FeedbackOptions
-            options={["good", "neutral", "bad"]}
-            onLeaveFeedback={this.countFeedback}
+  return (
+    <>
+      <Section title="Please leave Feedback">
+        <FeedbackOptions
+          options={["good", "neutral", "bad"]}
+          onLeaveFeedback={countFeedbacks}
+        />
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback() ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            feedbackPercentage={countPositiveFeedbackPercentage()}
           />
-        </Section>
-        <Section title="Statistics">
-          {total ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positivePercentage}
-            />
-          ) : (
-            <Notification />
-          )}
-        </Section>
-      </div>
-    );
-  }
+        ) : (
+          <Notification msg="No Feedbacks - Sorry" />
+        )}
+      </Section>
+    </>
+  );
 }
-
-export default App;
